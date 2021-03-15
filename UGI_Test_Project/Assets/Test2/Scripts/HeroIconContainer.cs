ccf1c7e25759dd3618cbfc5868ab6a7d026488cd
GooDimuch <GooDimuch@gmail.com>
2021-03-15 04:54:38 +0200

    create srollView hero icons

---
+++ UGI_Test_Project/Assets/Test2/Scripts/HeroIconContainer.cs
@@ -0,0 +1,11 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroIconContainer : MonoBehaviour {
+	public Button MainButton;
+	public Image HeroImage;
+	public Slider ExpSlider;
+	public TextMeshProUGUI LevelText;
+	public GameObject SelectedBorder;
+}
