df62386bb70c07ecd1daf7f905a5bc37e515589a
GooDimuch <GooDimuch@gmail.com>
2021-03-17 00:48:46 +0200

    Add attributes to utils

---
+++ UGI_Test_Project/Assets/Utils/Attributes/LayerAttribute.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class LayerAttribute : PropertyAttribute { }
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(LayerAttribute))]
+public class LayerAttributeEditor : PropertyDrawer {
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+		EditorGUI.BeginProperty(position, label, property);
+		property.intValue = EditorGUI.LayerField(position, label, property.intValue);
+		EditorGUI.EndProperty();
+	}
+}
+#endif
